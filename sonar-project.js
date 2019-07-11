f52eb84355cee9ffd5a756d2acaa65549bf08a87
akash0997 <50735938+akash0997@users.noreply.github.com>
2019-07-11 15:54:27 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -2,6 +2,9 @@ const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
        serverUrl: 'http://3.14.251.87:9000',
        options : {
+       'sonar.projectKey' : 'React-new'
+       'sonar.projectName' : 'React-new'
+       'sonar.projectVersion' : '0.0.1'
        'sonar.sources': '.',
        'sonar.inclusions' : 'src/**' // Entry point of your code
        }
